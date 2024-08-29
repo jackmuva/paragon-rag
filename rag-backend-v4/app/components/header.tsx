5ea99d050384c0b36653a33a5e546de4e6ea90f4
Jack Mu <jackmu@Jacks-MacBook-Pro.local>
2024-08-29 13:55:11 -0700

    have an endpoint for notion

--- rag-backend-v4/app/components/header.tsx
+++ rag-backend-v4/app/components/header.tsx
@@ -1,8 +1,5 @@
-// "use client";
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
-import { AuthenticatedConnectUser } from "@useparagon/connect";
 
 interface ChildProps {
   toggle: () => void
